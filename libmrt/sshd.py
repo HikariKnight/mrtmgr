5909285f0595030724b537bb3a701bb77676084b
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2019-07-17 23:16:30 +0200

    add $PATH and make universal nvram fallback using which

--- libmrt/sshd.py
+++ libmrt/sshd.py
@@ -1,7 +1,10 @@
 def set_ssh_key(args, profile):
     # Load profile info
     sshd_authkeys_key = profile['nvram'].get('sshd_authkeys_key',None)
-    nvram_bin = profile['router'].get('nvram_bin','/usr/sbin/nvram')
+    nvram_bin = profile['router'].get('nvram_bin','$(which nvram)')
+    PATH_env = profile['router'].get('PATH','/bin:/usr/bin:/sbin:/usr/sbin:/jffs/sbin:' +
+    '/jffs/bin:/jffs/usr/sbin:/jffs/usr/bin:/mmc/sbin:/mmc/bin:/mmc/usr/sbin:/mmc/usr/bin:' +
+    '/opt/sbin:/opt/bin:/opt/usr/sbin:/opt/usr/bin')
 
     # If no sshd_authkeys key is defined then error out and exit
     if not sshd_authkeys_key:
@@ -9,4 +12,4 @@ def set_ssh_key(args, profile):
         exit()
 
     # Return the router nvram command
-    return nvram_bin + " set " + sshd_authkeys_key + "=" + args.ssh_key
+    return PATH_env + ' ' + nvram_bin + " set " + sshd_authkeys_key + "=" + args.ssh_key
--- libmrt/webui.py
+++ libmrt/webui.py
@@ -1,7 +1,10 @@
 def set_http_port(args, profile):
     # Load profile info
     http_lanport_key = profile['nvram'].get('http_lanport_key',None)
-    nvram_bin = profile['router'].get('nvram_bin','/usr/sbin/nvram')
+    nvram_bin = profile['router'].get('nvram_bin','$(which nvram)')
+    PATH_env = profile['router'].get('PATH','/bin:/usr/bin:/sbin:/usr/sbin:/jffs/sbin:' +
+    '/jffs/bin:/jffs/usr/sbin:/jffs/usr/bin:/mmc/sbin:/mmc/bin:/mmc/usr/sbin:/mmc/usr/bin:' +
+    '/opt/sbin:/opt/bin:/opt/usr/sbin:/opt/usr/bin')
 
     # If no http_lanport key is defined then error out and exit
     if not http_lanport_key:
@@ -9,13 +12,13 @@ def set_http_port(args, profile):
         exit()
 
     # Return the router nvram command
-    return nvram_bin + " set " + http_lanport_key + "=" + str(args.http_port)
+    return PATH_env + ' ' + nvram_bin + " set " + http_lanport_key + "=" + str(args.http_port)
 
 
 def set_https_port(args, profile):
     # Load profile info
     https_lanport_key = profile['nvram'].get('https_lanport_key',None)
-    nvram_bin = profile['router'].get('nvram_bin','/usr/sbin/nvram')
+    nvram_bin = profile['router'].get('nvram_bin','$(which nvram)')
 
     # If no https_lanport key is defined then error out and exit
     if not https_lanport_key:
